2273604a1e41c72ddf51a68c2d946b749458acf4
JuliaDavidovich <julia_davidovich@mail.ru>
2024-09-26 10:51:46 +0300

    MapTasks, displayDigitsDistribution,getParethsesMaps

--- src/main/java/telran/collections/MapTasks.java
+++ src/main/java/telran/collections/MapTasks.java
@@ -1,36 +1,94 @@
 package telran.collections;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MapTasks {
-public static void displayOccurances(String[] strings) {
-    // input {"lpm", "ab", "a", "c", "cb", "cb", "c", "lpm", "lpm"}
+    public static void displayOccurances(String[] strings) {
+        // input {"lpm", "ab", "a", "c", "cb", "cb", "c", "lpm", "lpm"}
         // output:
         // lpm -> 3
         // c -> 2
         // cb -> 2
         // a -> 1
         // ab -> 1
-    HashMap<String, Long> occurancesMap = getMapOccurances(strings);
-    TreeMap<Long, TreeSet<String>> sortedOccurancesMap = getSortOccurancesMap(occurancesMap);
-    displaySortedOccurancesMap(sortedOccurancesMap);
-}
+        HashMap<String, Long> occurancesMap = getMapOccurances(strings);
+        TreeMap<Long, TreeSet<String>> sortedOccurancesMap = getSortOccurancesMap(occurancesMap);
+        displaySortedOccurancesMap(sortedOccurancesMap);
+    }
 
-private static void displaySortedOccurancesMap(TreeMap<Long, TreeSet<String>> sortedOccurancesMap) {
-    sortedOccurancesMap.forEach((occurancy, treeSet) -> 
-    treeSet.forEach(s -> System.out.printf("%s -> %d \n", s, occurancy)));
-}
+    public static void displayOccurrencesStream(String[] strings) {
+        Arrays.stream(strings).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().sorted((e1, e2) -> {
+                    int res = Long.compare(e2.getValue(), e1.getValue());
+                    return res == 0 ? e1.getKey().compareTo(e2.getKey()) : res;
+                }).forEach(e -> System.out.printf("%s -> %d\n", e.getKey(), e.getValue()));
+    }
 
-private static TreeMap<Long, TreeSet<String>> getSortOccurancesMap(HashMap<String, Long> occurancesMap) {
-    TreeMap<Long, TreeSet<String>> result = new TreeMap<>(Comparator.reverseOrder());
-    occurancesMap.entrySet().forEach(e -> result.computeIfAbsent(e.getValue(), 
-    k -> new TreeSet<>()).add(e.getKey()));
-    return result;
-}
+    private static void displaySortedOccurancesMap(TreeMap<Long, TreeSet<String>> sortedOccurancesMap) {
+        sortedOccurancesMap
+                .forEach((occurancy, treeSet) -> treeSet.forEach(s -> System.out.printf("%s -> %d \n", s, occurancy)));
+    }
 
-private static HashMap<String, Long> getMapOccurances(String[] strings) {
-    HashMap<String, Long> result = new HashMap<>();
-    Arrays.stream(strings).forEach(s -> result.merge(s, 1l, Long::sum));
-    return result; 
-}
+    private static TreeMap<Long, TreeSet<String>> getSortOccurancesMap(HashMap<String, Long> occurancesMap) {
+        TreeMap<Long, TreeSet<String>> result = new TreeMap<>(Comparator.reverseOrder());
+        occurancesMap.entrySet().forEach(e -> result.computeIfAbsent(e.getValue(),
+                k -> new TreeSet<>()).add(e.getKey()));
+        return result;
+    }
+
+    private static HashMap<String, Long> getMapOccurances(String[] strings) {
+        HashMap<String, Long> result = new HashMap<>();
+        Arrays.stream(strings).forEach(s -> result.merge(s, 1l, Long::sum));
+        return result;
+    }
+
+    public static Map<Integer, Integer[]> getGroupingByNumberOfDigits(int[][] array) {
+        // Arrays.stream(array).flatMapToInt(Arrays::stream).boxed() refactoe to
+        // streamOfNumbers
+        Map<Integer, List<Integer>> map = streamOfNumbers(array)
+                .collect(Collectors.groupingBy(n -> (Integer) (Integer.toString(n).length())));
+        return map.entrySet().stream().collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()
+                .toArray(Integer[]::new)));
+    }
+
+    private static Stream<Integer> streamOfNumbers(int[][] array) {
+        return Arrays.stream(array).flatMapToInt(Arrays::stream).boxed();
+    }
+
+    public static Map<Integer, Long> getDistributionByNumberOfDigits(int[][] array) {
+
+        return streamOfNumbers(array).collect(Collectors.groupingBy(n -> Integer.toString(n).length(),
+                Collectors.counting()));
+    }
+
+    public static void displayDigitsDistribution() {
+        // 1_000_000 random numbers from 0 to Integer.MAX_VALUE created
+        // Output should contain all digits (0 - 9) with counters of occurrences
+        // sorted by descending order of occurrences
+        // example:
+        // 1 -> <counter of occurrences>
+        // 2 -> <counter of occurrences>
+        // ..............
+        HashMap<Character, Long> digitsCounts = new HashMap<>();
+        new Random().ints().limit(1000000).forEach(number -> {
+            String numberString = String.valueOf(number);
+            numberString.chars().mapToObj(c -> (char) c).forEach(digit -> {
+                digitsCounts.merge(digit, 1l, Long::sum);
+            });
+        });
+        System.out.println(digitsCounts);
+    }
+
+    public static ParenthesesMaps getParenthesesMaps(Character[][] openCloseParentheses) {
+        Map<Character, Character> openCloseMap = new HashMap<>();
+        Map<Character, Character> closeOpenMap = new HashMap<>();
+        for(Character[] pair : openCloseParentheses) {
+            openCloseMap.put(pair[0], pair[1]);
+            closeOpenMap.put(pair[1], pair[0]);
+        }
+        return new ParenthesesMaps(openCloseMap, closeOpenMap);
+    }
 }
--- src/test/java/telran/collections/MapTasksTest.java
+++ src/test/java/telran/collections/MapTasksTest.java
@@ -2,10 +2,52 @@ package telran.collections;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
 public class MapTasksTest {
 @Test
 void displayOccurancesTest() {
     String[] strings = {"lpm", "ab", "a", "c", "cb", "cb", "c", "lpm", "lpm"};
     MapTasks.displayOccurances(strings);
+}
+@Test
+  void displayOccurrencesStreamTest(){
+    String[] strings = {"lpm", "ab", "a", "c", "cb", "cb", "c", "lpm", "lpm"} ;
+    MapTasks.displayOccurrencesStream(strings);
+  }
+@Test
+void getGroupingByNumberOfDigitsTest() {
+    int[][] array = {{100, 1, 50},{20, 30},{1}};
+    Map<Integer, Integer[]> map = MapTasks.getGroupingByNumberOfDigits(array);
+    Integer[] oneDigitNumbers = {1, 1};
+    Integer[] twoDigitsNumbers = {50, 20, 30};
+    Integer[] threeDigitsNumbers = {100};
+    assertArrayEquals(map.get(1), oneDigitNumbers);
+    assertArrayEquals(map.get(2), twoDigitsNumbers);
+    assertArrayEquals(map.get(3), threeDigitsNumbers);
+}
+@Test
+void getDistributionByNumberOfDigitsTest() {
+    int[][] array = {{100, 1, 50},{20, 30},{1}};
+    Map<Integer, Long> map = MapTasks.getDistributionByNumberOfDigits(array);
+    assertEquals(2, map.get(1));
+    assertEquals(3, map.get(2));
+    assertEquals(1, map.get(3));
+}
+@Test
+void digitsDistributionTest() {
+  MapTasks.displayDigitsDistribution();
+}
+@Test
+void getParenthesesMapsTest() {
+  Character[][] openCloseParentheses = {
+    {'[', ']'}, {'(', ')'}, {'{', '}'}
+  };
+  ParenthesesMaps maps = MapTasks.getParenthesesMaps(openCloseParentheses);
+  Map<Character, Character> openCloseMap = maps.openCloseMap();
+  Map<Character, Character> closeOpenMap = maps.closeOpenMap();
+  assertEquals(']', openCloseMap.get('['));
+  assertEquals('[', closeOpenMap.get(']'));
 }
 }
